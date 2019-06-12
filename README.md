122269c94d04d125435b7a66442ee084efd30873
Alan Chalk <alanchalk@gmail.com>
2019-06-12 23:17:09 +0100

    .gitignore fix

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-This is my **amazing** project.  And I learned lots of interesting stuff.
+# AML_2019 Coursework, Part 1, Group x_
+Experiments with Gradient Descent.
+---
+
+![logo](https://github.com/alanchalk/aml_2019_gd/master/images/04a_loss_function.pdf)
+
+## Introduction
+This is what this project is about
+
+## A section
+
+## Another section
+Now we discuss and illustrate the function.  We might include some links or references [my interesting reference](http://jupyter.org/)
+
+---
+## Experiments
+
+I might include some tables here and references to my code.
+
+| Parameter      | Explanation |
+|----------------|-------------|
+|`A`             | x           |
+|`B`             | y           |
+|`C`             | z           |
+
+---
+
--- gd.py
+++ gd.py
@@ -1,16 +1,12 @@
 import numpy as np
 
-class gd_pv_1d:
+class gd_1d:
     
     def __init__(self, fn_loss, fn_grad):
         self.fn_loss = fn_loss
         self.fn_grad = fn_grad
         
-    def find_min(self, x_init, n_iter, eta, tol):
-        #self.x_init = x_init
-        #self.n_iter = n_iter
-        #self.eta = eta
-        #self.tol = tol
+    def pv(self, x_init, n_iter, eta, tol):
         x = x_init
         
         loss_path = []
@@ -22,7 +18,7 @@ class gd_pv_1d:
         g = self.fn_grad(x)
 
         for i in range(n_iter):
-            if g < tol:
+            if np.abs(g) < tol or np.isnan(g):
                 break
             g = self.fn_grad(x)
             x += -eta * g
@@ -30,7 +26,79 @@ class gd_pv_1d:
             loss_this = self.fn_loss(x)
             loss_path.append(loss_this)
             
-        self.loss_path = loss_path
-        self.x_path = x_path
-        self.loss_fn_min = loss_this
-        self.x_at_min = x
+        if np.isnan(g):
+            print('Exploded')
+        elif np.abs(g) > tol:
+            print('Did not converge')
+        else:
+            print('Converged in {} steps.  Loss fn {} achieved by x = {}'.format(i, loss_this, x))
+        self.loss_path = np.array(loss_path)
+        self.x_path = np.array(x_path)
+        
+    def momentum(self, x_init, n_iter, eta, tol, alpha):
+        x = x_init
+        
+        loss_path = []
+        x_path = []
+        
+        x_path.append(x)
+        loss_this = self.fn_loss(x)
+        loss_path.append(loss_this)
+        g = self.fn_grad(x)
+        nu = 0
+
+        for i in range(n_iter):
+            g = self.fn_grad(x)
+            if np.abs(g) < tol or np.isnan(g):
+                break
+
+            nu = alpha * nu + eta * g
+            x += -nu
+            x_path.append(x)
+            loss_this = self.fn_loss(x)
+            loss_path.append(loss_this)
+
+        if np.isnan(g):
+            print('Exploded')
+        elif np.abs(g) > tol:
+            print('Did not converge')
+        else:
+            print('Converged in {} steps.  Loss fn {} achieved by x = {}'.format(i, loss_this, x))
+        self.loss_path = np.array(loss_path)
+        self.x_path = np.array(x_path)
+
+    def nag(self, x_init, n_iter, eta, tol, alpha):
+        x = x_init
+        
+        loss_path = []
+        x_path = []
+        
+        x_path.append(x)
+        loss_this = self.fn_loss(x)
+        loss_path.append(loss_this)
+        g = self.fn_grad(x)
+        nu = 0
+
+        for i in range(n_iter):
+            # i starts from 0 so add 1
+            # The formula for mu was mentioned by David Barber UCL as being Nesterovs suggestion
+            mu = 1 - 3 / (i + 1 + 5) 
+            g = self.fn_grad(x - mu*nu)
+            if np.abs(g) < tol or np.isnan(g):
+                break
+
+            nu = alpha * nu + eta * g
+            x += -nu
+            x_path.append(x)
+            loss_this = self.fn_loss(x)
+            loss_path.append(loss_this)
+
+        if np.isnan(g):
+            print('Exploded')
+        elif np.abs(g) > tol:
+            print('Did not converge')
+        else:
+            print('Converged in {} steps.  Loss fn {} achieved by x = {}'.format(i, loss_this, x))
+        self.loss_path = np.array(loss_path)
+        self.x_path = np.array(x_path)
+        
